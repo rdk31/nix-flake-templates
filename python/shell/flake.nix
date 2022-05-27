5f95fa6dda89354c837b0561235439eb30624788
rdk31 <rdk31@protonmail.com>
2022-05-27 13:31:12 +0200

    python mach: specify pypi deps; github pkgs

--- python/shell/flake.nix
+++ python/shell/flake.nix
@@ -5,6 +5,11 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.mach-nix.url = "github:davhau/mach-nix";
+  # inputs.pypi-deps-db = {
+  #   url = "github:davhau/pypi-deps-db/0000000000000000000000000000000000000000";
+  #   flake = false;
+  # };
+  # inputs.mach-nix.inputs.pypi-deps-db.follows = "pypi-deps-db";
 
   outputs = { self, nixpkgs, flake-utils, mach-nix, ... }:
     let
@@ -19,6 +24,16 @@
           pythonEnv = mach.mkPython {
             python = pythonVersion;
             requirements = builtins.readFile ./requirements.txt;
+            # packagesExtra = [
+            #   (mach.buildPythonPackage
+            #     {
+            #       src = builtins.fetchGit {
+            #         url = "https://github.com/user/repo";
+            #         ref = "branch";
+            #         rev = "0000000000000000000000000000000000000000";
+            #       };
+            #     })
+            # ];
           };
         in
         {
